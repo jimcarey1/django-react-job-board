363eef211924106b947a739ae678b759ea70d6da
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-04 12:15:35 +0530

    Changed from axios to fetch to make the request to the backend.

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -7,12 +7,19 @@ export const AuthContext = createContext();
 export function AuthProvider({ children }) {
   const [accessToken, setAccessToken] = useState(null);
 
-  const login = async ({ username, password }) => {
-    const res = await axios.post("http://localhost:8000/api/auth/login", { username, password }, { withCredentials: true });
-    if (res.data.access) {
-      setAccessToken(res.data.access);
+  const login = async ({ email, password }) => {
+    const res = await fetch("http://localhost:8000/api/auth/login",{
+      method: 'POST',
+      headers:{
+        'Content-Type':'application/json',
+      },
+      body: JSON.stringify({"email":email, "password":password})
+    });
+    const data = await res.json()
+    if (data.access) {
+      setAccessToken(data.access);
       // optionally store refresh in localStorage if returned (less secure)
-      if (res.data.refresh) localStorage.setItem("refresh", res.data.refresh);
+      if (data.refresh) localStorage.setItem("refresh", data.refresh);
       return true;
     }
     return false;
