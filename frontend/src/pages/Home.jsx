9150393150eb017a03125d54018648cdfcd37a74
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-04 12:17:09 +0530

    Removed Drop down over register and login buttons
    In the register page, you have both options either register with username and password or sign in with google.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -4,16 +4,6 @@ import '../css/navbar.css'
 
 const Home = ()=>{
     const {accessToken, setAccessToken, login, logout} = useContext(AuthContext);
-    const [openRegister, setOpenRegister] = useState(false);
-    const [openLogin, setOpenLogin] = useState(false);
-
-    const toggleRegisterButton = ()=>{
-        setOpenRegister(!openRegister)
-    }
-
-    const toggleLoginButton = ()=>{
-        setOpenLogin(!openLogin)
-    }
 
     if(accessToken){
         //User is already logged in.
@@ -26,24 +16,8 @@ const Home = ()=>{
                     <p>Django Job Board</p>
                 </div>
                 <div className="auth-related">
-                    <div className="dropdown-container">
-                        <button className="register" onClick={toggleRegisterButton}>Register</button>
-                            {openRegister && (
-                                    <div className="dropdown-menu">
-                                        <button>Sign up with google</button>
-                                        <button>Sign up with username and password</button>
-                                    </div>
-                                )}
-                    </div>
-                    <div className="dropdown-container">
-                        <button className="login" onClick={toggleLoginButton}>Login</button>
-                            {openLogin && (
-                                    <div className="dropdown-menu">
-                                        <button>Sign in with google</button>
-                                        <button>Sign in with username and password</button>
-                                    </div>
-                                )}
-                    </div>
+                    <button className="register">Register</button>
+                    <button className="login">Login</button>
                 </div>
             </div>
             </>
