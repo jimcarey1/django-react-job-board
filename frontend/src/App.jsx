7cb846be88a8e8662299680e0a326d580eccb54d
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-04 12:22:09 +0530

    configured the routes with react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
-import GoogleLoginButton from './components/GoogleLoginButton'
+import {Route, Routes} from "react-router-dom"
+
 import Home from './pages/Home'
+import LoginForm from "./components/LoginForm"
+import RegisterForm from "./components/RegistrationForm"
 
 function App() {
   return (
     <>
-    <Home/>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/register" element={<RegisterForm />} />
+    </Routes>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,14 @@ import App from './App.jsx'
 import './css/main.css'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { AuthProvider } from './context/AuthContext.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <GoogleOAuthProvider clientId='258088388347-o1gp9ofno79k658bheoltk68tut7n794.apps.googleusercontent.com'>
     <AuthProvider>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </AuthProvider>
   </GoogleOAuthProvider>
 )
