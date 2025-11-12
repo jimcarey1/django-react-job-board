a77d63d51a5d51db53c4259437dba33bed392d98
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-12 14:23:19 +0530

    Made logout button functional.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,5 +1,10 @@
+import { useContext } from "react"
+import { AuthContext } from "../context/AuthContext"
+import { Link } from "react-router-dom"
+
 
 const Navbar = ()=>{
+    const {logout} = useContext(AuthContext)
     const accessToken = localStorage.getItem('access')
 
     if(accessToken){
@@ -10,7 +15,7 @@ const Navbar = ()=>{
                     <p>Django Job Board</p>
                 </div>
                 <div className="auth-related">
-                    <button className="logout">Logout</button>
+                    <button className="logout" onClick={logout}>Logout</button>
                     <div className="profile">
                         <i className="bi bi-person-circle"></i>
                     </div>
