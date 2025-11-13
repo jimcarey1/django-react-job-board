57d6f82df3906f53f36a26ed8d08208078308d4a
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-13 18:46:09 +0530

    Added a form to add skill for a user.
    When the user add the skill and hit the submit button, It will send a post
    request to the backend endpoint that adds skill to the db.
    
    Configured styling.

--- frontend/src/components/AddSkills.jsx
+++ frontend/src/components/AddSkills.jsx
@@ -1,6 +1,41 @@
+import { useRef } from 'react'
 import '../css/addexperience.css'
 
 const AddSkills = () =>{
+    const skillFormRef = useRef(null)
+    const accessToken = localStorage.getItem('access') || null 
+
+    const handleAddSkill = ()=>{
+        if(skillFormRef.current){
+            skillFormRef.current.style.display = 'flex'
+            skillFormRef.current.style.flexDirection = 'column'
+            skillFormRef.current.style.gap = '10px'
+        }
+    }
+
+    const addSkill = async (formData)=>{
+        const name = formData.get('name')
+        if(name){
+            formData = {
+                "name" : name
+            }
+        }
+
+        const response = await fetch('http://localhost:8000/api/experience/add-skill', {
+            method: 'POST', 
+            headers:{
+                "Content-Type" : "application/json",
+                'Authorization': `Bearer ${accessToken}`,
+            },
+            credentials: 'include',
+            body: JSON.stringify(formData)
+        })
+        if(response.ok){
+            const data = await response.json()
+            console.log(data)
+        }
+    }
+
     return (
         <>
         <div className="add-skills">
@@ -15,8 +50,19 @@ const AddSkills = () =>{
                 </div>
             </div>
             <div>
-                <button className="skill-button">Add Skill</button>
+                <button className="skill-button" onClick={handleAddSkill}>Add Skill</button>
             </div>
+        </div>
+        <div className='skill-form' ref={skillFormRef}>
+            <form className='add-skill-form' action={addSkill}>
+                <div className='field' id='name-field'>
+                    <label>Skill*</label>
+                    <input type='text' id='add-skill' name='name' required />
+                </div>
+                <div>
+                    <button className='add-skill-button'>Add Skill</button>
+                </div>
+            </form>
         </div>
         </>
     )
