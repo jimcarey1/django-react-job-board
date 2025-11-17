d017664faf82bc1cc6b813f4bdb703cfb394a4df
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-17 14:17:06 +0530

    removed code that is not useful and make the APIs async compatible.
    Removed 'request.user = user' line as it does not do anything.

--- backend/accounts/api.py
+++ backend/accounts/api.py
@@ -3,12 +3,13 @@ from ninja import Router, Schema
 from ninja.responses import Response
 from ninja.errors import HttpError
 from django.http import HttpRequest
-from django.contrib.auth import authenticate
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, aauthenticate
+from django.forms import model_to_dict
 from ninja_jwt.tokens import RefreshToken
 from google.oauth2 import id_token as google_id_token
 from google.auth.transport import requests as google_requests
 from django.conf import settings
+from asgiref.sync import sync_to_async
 
 import jwt
 from jwt.exceptions import ExpiredSignatureError
@@ -30,25 +31,27 @@ class TokenOut(Schema):
     access: str
 
 @router.post("/register", response=TokenOut)
-def register(request: HttpRequest, data: RegisterIn):
-    response = Response('Response object')
-    if User.objects.filter(email=data.email).exists():
+async def register(request: HttpRequest, data: RegisterIn):
+    if await User.objects.filter(email = data.email).aexists():
         raise HttpError(status_code=400, message='A User with this email already exists.')
-    user = User.objects.create_user(email=data.email, password=data.password, first_name=data.first_name, last_name=data.last_name)
-    refresh = RefreshToken.for_user(user)
+    user = User(email=data.email, first_name=data.first_name, last_name=data.last_name)
+    user.set_password(data.password)
+    await user.asave()
+    refresh = await sync_to_async(RefreshToken.for_user)(user)
+    access = str(refresh.access_token)
+    response = Response({"access": access, "user":model_to_dict(user, fields=['id', 'email', 'first_name', 'last_name'])})
     response.set_signed_cookie('refresh_token', str(refresh), settings.SALT, max_age=7*24*60*60)
-    request.user = user
-    return {"access": str(refresh.access_token)}
+    return response 
 
 @router.post("/login")
-def login(request: HttpRequest, data: LoginIn):
-    user = authenticate(email=data.email, password=data.password)
+async def login(request: HttpRequest, data: LoginIn):
+    user = await aauthenticate(email=data.email, password=data.password)
     if not user:
         raise HttpError(status_code=400, message='Invalid credentials.')
-    refresh = RefreshToken.for_user(user)
-    response = Response({"access": str(refresh.access_token)})
+    refresh = await sync_to_async(RefreshToken.for_user)(user)
+    access = str(refresh.access_token)
+    response = Response({"access": access, "user":model_to_dict(user, fields=['id', 'email', 'first_name', 'last_name'])})
     response.set_signed_cookie('refresh_token', str(refresh), settings.SALT, max_age=7*24*60*60)
-    request.user = user
     return response
 
 # Google social login: frontend sends id_token (from Google Identity)
@@ -56,23 +59,27 @@ class GoogleTokenIn(Schema):
     id_token: str 
 
 @router.post("/google/", response=TokenOut)
-def google_login(request: HttpRequest, data:GoogleTokenIn):
+async def google_login(request: HttpRequest, data:GoogleTokenIn):
     try:
         # Verify the token with Google's public keys
-        idinfo = google_id_token.verify_oauth2_token(data.id_token, google_requests.Request(), settings.GOOGLE_CLIENT_ID)
+        idinfo = await sync_to_async(google_id_token.verify_oauth2_token) \
+                        (data.id_token, 
+                         google_requests.Request(), 
+                         settings.GOOGLE_CLIENT_ID
+                        )
         # idinfo contains email, sub (user id), name, picture...
         email = idinfo.get("email")
         first_name = idinfo.get('given_name')
         last_name = idinfo.get('family_name')
         if not email:
             raise Exception("No email in ID token")
-        user, created = User.objects.get_or_create(email=email, first_name=first_name, last_name=last_name)
+        user, created = await User.objects.aget_or_create(email=email, first_name=first_name, last_name=last_name)
         # Optionally set other fields if created
-        refresh = RefreshToken.for_user(user)
-        response = Response({"access": str(refresh.access_token)})
+        refresh = await sync_to_async(RefreshToken.for_user)(user)
+        access_token = str(refresh.access_token)
+        response = Response({"access": access_token, "user": model_to_dict(user, fields=['id', 'email', 'first_name', 'last_name'])})
         response.status_code = 200
-        response.set_signed_cookie('refresh_token', refresh, settings.SALT, max_age=7*24*60*60)
-        request.user = user
+        response.set_signed_cookie('refresh_token', str(refresh), settings.SALT, max_age=7*24*60*60)
         return response
     except Exception as e:
         # token invalid or verification failed
@@ -81,12 +88,10 @@ def google_login(request: HttpRequest, data:GoogleTokenIn):
 
 
 @router.post('/access_token', response=TokenOut)
-def new_access_token(request: HttpRequest, data:TokenOut):
+async def new_access_token(request: HttpRequest, data:TokenOut):
     access_token = data.access
     try:
-        payload = jwt.decode(access_token, settings.NINJA_JWT['SIGNING_KEY'], settings.NINJA_JWT['ALGORITHM'])
-    except ExpiredSignatureError:
-        pass
+        payload = jwt.decode(access_token, settings.NINJA_JWT['SIGNING_KEY'], settings.NINJA_JWT['ALGORITHM'], options={'verify_exp':False})
     except Exception as e:
         raise HttpError(status_code=400, message='Invalid Access Token')
     refresh_token:str = request.get_signed_cookie('refresh_token', salt=settings.SALT)
@@ -96,7 +101,7 @@ def new_access_token(request: HttpRequest, data:TokenOut):
         refresh_token = RefreshToken(refresh_token)
         return {'access': str(refresh_token.access_token)}
     except Exception as e:
-        return HttpError(status_code=400, message=str(e))
+        raise HttpError(status_code=400, message=str(e))
 
 
 @router.get('/logout')
