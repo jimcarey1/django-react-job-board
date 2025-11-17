0d88790ddae656952ebc11a576e13feb5fce2f03
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-11-17 14:58:34 +0530

    Getting user object from the AuthContext and displaying user info on profile page.

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,17 +1,20 @@
+import { useContext } from "react"
 import AddExperience from "../components/AddExperience"
 import AddSkills from "../components/AddSkills"
 import '../css/profile.css'
+import {AuthContext} from "../context/AuthContext"
 
 const Profile = () => {
-    const fullName = `first Last`
+    const {user} = useContext(AuthContext)
+    const fullName = `${user.first_name} ${user.last_name}`
     const userDescription = "Backend Engineer at Amazon"
 
     return(
         <>
         <div className="user-profile">
             <div className="user-intro">
-                <p className="name">fullName</p>
-                <p className="desc">userDescription</p>
+                <p className="name">{fullName}</p>
+                <p className="desc">{userDescription}</p>
             </div>
             <AddExperience />
             <AddSkills />
