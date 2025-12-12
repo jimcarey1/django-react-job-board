e53f304022de0acce2c06ebd73cc291befad5f37
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-12-12 13:55:57 +0530

    The Page component now renders dynamic content according to the company.

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -1,11 +1,45 @@
 import { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
 import '../css/page.css'
 
 const Page = ()=>{
-    
+    const accessToken = localStorage.getItem('access') || null;
+    const {name} = useParams()
+    const [company, setCompany] = useState('');
+    useEffect(()=>{
+        const fetchCompany = async ()=>{
+            try{
+                const response = await fetch(`http://localhost:8000/api/company/${name}/page`, {
+                    method: 'GET',
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${accessToken}`,
+                    },
+                    credentials: 'include'
+                })
+                if(response.ok){
+                    try{
+                        const data = await response.json()
+                        setCompany(data.page)
+                    }catch(error){
+                        console.log(error)
+                    }
+                }
+            }catch(error){
+                console.log(error)
+            }
+        }
+        fetchCompany()
+    }, [])
     return (
         <>
-        <h1>Company Page</h1>
+        <div className='company-page' id={company.id}>
+            <div>
+                <span>{company.title}</span>
+                <span>{company.url}</span>
+                <span>Specialized in: {company.specialization}</span> 
+            </div>
+        </div>
         </>
     )
 }
