ac17ff7eb2af8319d005cbc18b3536615871b196
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-12-12 13:53:10 +0530

    Added an API to fetch a company page based on the title.

--- backend/pages/api.py
+++ backend/pages/api.py
@@ -6,6 +6,7 @@ from django.http import HttpRequest
 from django.forms import model_to_dict
 from django.contrib.auth import get_user_model
 from django.conf import settings
+from django.core.exceptions import ObjectDoesNotExist
 from typing import List
 import jwt
 from jwt.exceptions import ExpiredSignatureError, InvalidTokenError
@@ -13,6 +14,7 @@ from asgiref.sync import sync_to_async
 
 from .models import Specialization, COMPANY_SIZE, Organization
 from .utils import get_specializations_list
+from accounts.api import UserSchema
 
 router = Router()
 User = get_user_model()
@@ -31,8 +33,18 @@ class CreatePageIn(Schema):
     company_size: str
     overview: str
 
+class OrganizationSchema(Schema):
+    id: int
+    title: str
+    specialization: str
+    url:str
+    headquarters: str
+    company_size: str
+    overview: str
+    user: UserSchema
 
-@router.post('/create-page')
+
+@router.post('/create-page', response=OrganizationSchema)
 async def create_page(request:HttpRequest, data:CreatePageIn):
     authorization_header:str = request.headers.get('Authorization')
     if authorization_header and authorization_header.startswith('Bearer'):
@@ -49,7 +61,7 @@ async def create_page(request:HttpRequest, data:CreatePageIn):
         user = await User.objects.aget(id=user_id)
         if(not data.name or not data.specialization or not data.company_size or not data.headquarters or not data.url or not data.overview):
             raise HttpError(status_code=400, message='Invalid form information')
-        organization = await Organization.objects.acreate(
+        company = await Organization.objects.acreate(
             title = data.name,
             specialization = data.specialization,
             url = data.url,
@@ -58,26 +70,23 @@ async def create_page(request:HttpRequest, data:CreatePageIn):
             headquarters = data.headquarters,
             user = user
         )
-        organization_dict = model_to_dict(organization, fields=['title', 'specialization', 'url', 'overview', 'headquarters', 'company_size'])
-        response = Response({'company':organization_dict})
-        response.status_code = 200
-        return response
+        return company
     except Exception as exc:
         print(str(exc))
         raise HttpError(status_code=500, message=str(exc))
-    
-        
 
-@router.get('/get-page')
-async def get_page():
-    pass
+@router.get('/{name}/page', response=OrganizationSchema)
+async def get_page(request:HttpRequest, name:str):
+    try:
+        page = await Organization.objects.select_related('user').aget(title=name)
+        return page
+    except ObjectDoesNotExist:
+        raise HttpError(status_code=404, message='Not found')
 
 @router.get('/specializations', response=SpecializationOut)
 async def get_specializations(request:HttpRequest):
-    specializations = await sync_to_async(get_specializations_list)(Specialization)
-    response = Response({'specializations':specializations})
-    response.status_code = 200
-    return response
+    specializations = await sync_to_async(Specialization.objects.all)
+    return specializations
 
 @router.get('/company-size')
 async def get_company_sizes(request:HttpRequest):
