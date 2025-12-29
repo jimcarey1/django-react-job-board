52b303423a634453be3e0fde2ec805c71d187f4e
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-12-29 20:23:25 +0530

    Added an optional prop "job", when we pass the job object, It does not call to backend API, if the job is null It will get the job from the backend API.

--- frontend/src/pages/Job.jsx
+++ frontend/src/pages/Job.jsx
@@ -1,40 +1,44 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
-const Job = ()=>{
+const Job = ({job_=null})=>{
+    console.log(`The job is ${job_.id}`)
     //checking for accessToken in the localStorage.
     const accessToken = localStorage.getItem('access') || null;
 
     const {name, id} = useParams() 
-    const [job, setJob] = useState(null)
+    const [job, setJob] = useState(job_)
 
-    //We are fetching the job when the component renders.
-    useEffect(()=>{
-        const fetchJob = async ()=>{
-            try{
-                const response = await fetch(`http://localhost:8000/api/company/job/${name}/${id}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${accessToken}`,
-                    },
-                    credentials: 'include'
-                })
-                if(response.ok){
-                    try{
-                        const jobData = await response.json()
-                        console.log(jobData)
-                        setJob(jobData)
-                    }catch(error){
-                        console.log(error)
+    if(!job){
+        console.log('useEffect running')
+        //We are fetching the job when the component renders.
+        useEffect(()=>{
+            const fetchJob = async ()=>{
+                try{
+                    const response = await fetch(`http://localhost:8000/api/company/job/${name}/${id}`, {
+                        method: 'GET',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Authorization': `Bearer ${accessToken}`,
+                        },
+                        credentials: 'include'
+                    })
+                    if(response.ok){
+                        try{
+                            const jobData = await response.json()
+                            console.log(jobData)
+                            setJob(jobData)
+                        }catch(error){
+                            console.log(error)
+                        }
                     }
+                }catch(error){
+                    console.log(error)
                 }
-            }catch(error){
-                console.log(error)
             }
-        }
-        fetchJob()
-    }, [])
+            fetchJob()
+        }, [])
+    }
 
     //Rendering the job details on the page.
     return (
@@ -43,7 +47,7 @@ const Job = ()=>{
                 {job && 
                 <>
                     <span>{job.title}</span>
-                    <span>Posted by {job.organization.title}</span>
+                    {/**<span>Posted by {job.organization.title}</span>*/}
                     {job.description}
                 </>}
             </div>
