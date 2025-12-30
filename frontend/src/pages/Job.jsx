359bf8fba4824d994ca2c397ea1deeb8a6190e69
jimcarey1 <89066752+jimcarey1@users.noreply.github.com>
2025-12-30 20:36:12 +0530

    Made the job page more detailed.

--- frontend/src/pages/Job.jsx
+++ frontend/src/pages/Job.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 
+import '../css/job.css'
+
 const Job = ({job_=null})=>{
     console.log(`The job is ${job_.id}`)
     //checking for accessToken in the localStorage.
@@ -10,7 +12,6 @@ const Job = ({job_=null})=>{
     const [job, setJob] = useState(job_)
 
     if(!job){
-        console.log('useEffect running')
         //We are fetching the job when the component renders.
         useEffect(()=>{
             const fetchJob = async ()=>{
@@ -44,12 +45,30 @@ const Job = ({job_=null})=>{
     return (
         <div className="job-page">
             <div className="job-details">
-                {job && 
-                <>
-                    <span>{job.title}</span>
-                    {/**<span>Posted by {job.organization.title}</span>*/}
-                    {job.description}
-                </>}
+                <div className="basic-info">
+                    <span className="job-title">{job.title}</span>
+                    <span className="posted-by">Posted by <span className="org-title">{job.organization.title}</span></span>
+                </div>
+                <div className="job-desc">
+                    <p className="job-desc-heading">Job Description</p>
+                    <span>{job.description}</span>
+                </div>
+                <div className="skills">
+                    <span className="req-skills-heading">Required skills: </span>
+                    <div>
+                        {job.skills.map((skill)=>(
+                            <span>{skill.name}, </span>
+                        ))}
+                    </div>
+                </div>
+                <div className="experience">
+                    <span className="req-experience-heading">Experience </span>
+                    <span>We need a candidate that has an experience of atleast: {job.experience} years.</span>
+                </div>
+                <div className="ctc">
+                    <span className="provided-ctc-heading">Provided CTC</span>
+                    <span>CTC: {job.ctc}</span>
+                </div>
             </div>
         </div>
     )
